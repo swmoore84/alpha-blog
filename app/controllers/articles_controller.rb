54752012fba0e4dea00c87818afd56299226228d
Sam Moore <swmoore84@gmail.com>
2020-04-19 22:54:49 +0900

    added create function and strong params to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,5 +12,17 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
