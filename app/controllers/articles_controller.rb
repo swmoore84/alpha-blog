05f48d3f22cdd8d848ff7f5011218b1d883ef26d
Sam Moore <swmoore84@gmail.com>
2020-04-20 12:45:14 +0900

    added edit and update for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -19,6 +23,12 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to @article
   end
 
   private
