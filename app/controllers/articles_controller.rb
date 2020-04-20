00ac5a53a8523b561950c413c68b8d3362f8d3a2
Sam Moore <swmoore84@gmail.com>
2020-04-20 23:34:44 +0900

    added custom styling to homepage

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,6 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was successfully created"
       redirect_to @article
     else
       render :new
@@ -27,7 +26,6 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully updated"
       redirect_to @article
     else
       render :edit
