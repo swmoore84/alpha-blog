928bfe2aa5039ec52840b3df365dd9f38a6c08c8
Sam Moore <swmoore84@gmail.com>
2020-04-22 22:06:15 +0900

    Added edit for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def user_params
