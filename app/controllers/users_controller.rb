d28b1bf8f9f1bb8e943bc648bbf8119fa8b45c68
Sam Moore <swmoore84@gmail.com>
2020-04-22 23:06:55 +0900

    made style changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to articles_path
+      redirect_to @user
     else
       render :new
     end
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to articles_path
+      redirect_to @user
     else
       render :edit
     end
