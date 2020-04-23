5b3f7f1de3aa7ac3f6cb47edee74aa659749e72c
Sam Moore <swmoore84@gmail.com>
2020-04-23 16:51:34 +0900

    added login feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_path
   end
 end
